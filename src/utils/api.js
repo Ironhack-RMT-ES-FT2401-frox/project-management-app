0e3b12d1d2fea7e94c5e1f38a336e387b4079025
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2024-02-27 14:30:23 +0100

    ajustes para deploy

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,4 @@
-const API_URL = "https://project-management-api-4641927fee65.herokuapp.com"
+// const API_URL = "https://project-management-api-4641927fee65.herokuapp.com"
+const API_URL = import.meta.env.VITE_SERVER_URL
 
 export default API_URL
